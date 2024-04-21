7837fbf473388ca62ad130bc9b4a19d95415ced5
varstab <dan.vartsab@gmail.com>
2024-04-20 22:16:09 -0400

    Added error handler

--- 1.py
+++ 1.py
@@ -1,7 +1,13 @@
 from datetime import datetime as dt
 
 def get_days_from_today(date):
-    delta = dt.today() - dt.strptime(date, "%Y-%m-%d")
-    return delta.days
+    try:
+        # Trying to parse the input into datettime object
+        # and find the difference with todays date.
+        delta = dt.today() - dt.strptime(date, "%Y-%m-%d")
+        return delta.days
+    except ValueError:
+        # The wrong input format will cause ValueError
+        return "Invalid input! The input must follow the pattern YYYY-MM-DD"
 
 print(get_days_from_today("2024-04-22"))
