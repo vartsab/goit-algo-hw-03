546fa9934ed73253a425de9792f7eb16a9752227
varstab <dan.vartsab@gmail.com>
2024-04-20 22:04:33 -0400

    Upd. Res as a sorted list

--- 2.py
+++ 2.py
@@ -1,12 +1,17 @@
 import random as rn
 
 def get_numbers_ticket(min, max, quantity):
+    #Перевірка валідності вводу
     if (min < 1) or (max < min) or (max > 1000) or ((max - min - quantity) < -1):
         return "Не вірно задані вхідні параметри лотереї!\n" \
                "Будь-ласка впевніться, що 0 < min < max < 1000."
-    res=set()
+    #Декларую множину res для зберігання лише унікальних даних
+    res = set()
+    #Генерую випадкові числа та додаю їх множини допоки список
+    #не буде мати потрібну довжину
     while len(res) < quantity:
         res.add(rn.randint(min,max))
-    return res
+    #Повертаю результат роботи функції у вигляді відсортованого списка
+    return sorted (res)
 
 print(get_numbers_ticket(1,6,6))
